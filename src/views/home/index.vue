9afeaccc29f11f75581902611ac568c7cf3de176
xlin021 <516032842@qq.com>
2022-03-30 23:24:55 +0800

    chore: 配置模块路径别名

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,14 +2,22 @@
   <h1>Home</h1>
   <!-- <h1>{{ $store.state.count }}</h1> -->
   <h1>{{ store.state.count }}</h1>
+  <img src="@/assets/logo.png">
+  <div class="box" />
 </template>
 
 <script lang='ts' setup>
-import { useStore } from '../../store'
+import { useStore } from '@/store'
 
 const store = useStore()
 console.log(store.state.count)
 
 </script>
 
-<style scoped lang='scss'></style>
+<style scoped lang='css'>
+.box {
+  width: 200px;
+  height: 200px;
+  background: url('@/assets/logo.png');
+}
+</style>
