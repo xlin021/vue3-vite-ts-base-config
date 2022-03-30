acfb50a7b24b34dcd4cc3661b04d1f98c5ee0062
xlin021 <516032842@qq.com>
2022-03-30 22:27:46 +0800

    chore: 代码规范和 Eslint-在开发和构建的时候进行验证

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     // 'plugin:vue/essential',
     'plugin:vue/vue3-strongly-recommended',
-    'airbnb-base',
+    'airbnb-base'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
     'vue',
-    '@typescript-eslint',
+    '@typescript-eslint'
   ],
   rules: {
+    'comma-dangle': ['error', 'never'],
     semi: ['error', 'never'],
-  },
+    'import/no-extraneous-dependencies': 'off'
+  }
 }
