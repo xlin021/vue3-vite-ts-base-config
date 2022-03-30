4a2db242c9fbd75c0d999e3041742085d2395106
xlin021 <516032842@qq.com>
2022-03-30 23:41:34 +0800

    chore: 接口类型-提取接口类型模块

--- src/api/common.ts
+++ src/api/common.ts
@@ -2,11 +2,10 @@
  * 公共基础请求模块
  */
 import request from '@/utils/request'
+import { ILoginInfo } from './types/common'
 
 // eslint-disable-next-line import/prefer-default-export
-export const getLoginInfoService = () => request<{
-  login_logo: string
-}>({
+export const getLoginInfoService = () => request<ILoginInfo>({
   method: 'GET',
   url: '/login/info'
-})
+}).then(res => res.data)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,12 +4,16 @@
 
 <script lang='ts' setup>
 import { getLoginInfoService } from '@/api/common'
-import { onMounted } from 'vue'
+// 引入类型需要加 type 说明
+import type { ILoginInfo } from '@/api/types/common'
+import { onMounted, ref } from 'vue'
+
+const userInfo = ref<ILoginInfo | null>(null)
 
 onMounted(() => {
   getLoginInfoService().then(data => {
-    // console.log(res.data.data.login_logo)
-    console.log(data.message)
+    console.log(data)
+    userInfo.value = data
   })
 })
 </script>
